aae044e916e8765abee424cca7b7a2862466262d
roalcantara <rogerio.alcantara@mail.com>
2020-07-07 03:45:03 -0300

    feat(completion): Add heroku competions

---
+++ modules/heroku/README.md
@@ -0,0 +1,7 @@
+# Heroku
+
+Setup Heroku CLI
+
+*The authors of this module should be contacted via the [issue tracker][4].*
+
+- [roalcantara](https://github.com/roalcantara)
---
+++ modules/heroku/init.zsh
@@ -0,0 +1,10 @@
+#
+# Setup heroku
+#
+# Authors:
+#   roalcantara <rogerio.alcantara@gmail.com>
+#
+
+if [[ -s "$HEROKU_AC_HOME=/zsh_setup" ]]; then
+  source "$HEROKU_AC_HOME=/zsh_setup"
+fi

c036b1862c0310d48862b4efb5999d1853e023a2
roalcantara <rogerio.alcantara@gmail.com>
2020-10-24 04:17:55 -0300

    feat(modules): Add fast-syntax-highlighting
    (Short name F-Sy-H). Syntax-highlighting for Zshell – fine
    granularity, number of features, 40 work hours themes.
    
    More: https://github.com/zdharma/fast-syntax-highlighting

---
+++ modules/fast-syntax-highlighting/README.md
@@ -0,0 +1,9 @@
+# fast-syntax-highlighting
+
+Feature rich syntax highlighting for Zsh.
+
+## Authors
+
+_The authors of this module should be contacted via the [issue tracker][2]._
+
+- [@roalcantara](https://github.com/roalcantara)
---
+++ modules/fast-syntax-highlighting/init.zsh
@@ -0,0 +1,57 @@
+#
+# Setup fast-syntax-highlighting
+#
+# Authors:
+#   roalcantara <rogerio.alcantara@gmail.com>
+#
+
+# di directories
+# ex executable files
+# fi regular files
+# ln symlinks
+# ur,uw,ux user permissions
+# gr,gw,gx group permissions
+# tr,tw,tx others permissions
+# sn the numbers of a file's size
+# sb the units of a file's size
+# uu user that is you
+# un user that is someone else
+# gu a group that you belong to
+# gn a group you aren't a member of
+# ga new file in Git
+# gm a modified file in Git
+# gd a deleted file in Git
+# gv a renamed file in Git
+# da a file's date
+
+export EXA_COLORS="\
+fi=34:\
+di=1;36:\
+sn=32:\
+sb=32:\
+uu=1;36:\
+un=38;5;160:\
+gu=38;5;40:\
+gn=38:5:160:\
+bl=38;5;220:\
+ur=34:\
+uw=1;34:\
+ux=1;35:\
+ue=1;35:\
+ga=36:\
+gm=33:\
+gd=31:\
+gv=33:\
+gt=37:\
+gr=34:\
+gw=1;34:\
+gx=1;35:\
+tr=34:\
+tw=1;34:\
+tx=1;35:\
+su=37:\
+sf=37:\
+lc=37:"
+
+# Source module files.
+source "${0:h}/external/fast-syntax-highlighting.plugin.zsh" || return 1
